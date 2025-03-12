a432ea317d92da90b6d465756973d3d747b7810c
Daniel Johnson <voltron42@users.noreply.github.com>
2025-03-12 16:09:56 +0000

    stubbing out facial options for proof of concept

---
+++ poc/scripts/faces.js
@@ -0,0 +1,57 @@
+namespace("fm.poc.Faces", {}, () => {
+  const bodyTypes = ["fit","superman","hulk","woman"];
+  const ranges = {
+    eyebrows: [27,47],
+    eyes: [41,48],
+    mouth: [0,26]
+  }
+  const buildFace = function(bodyType, mouth, rightEyebrow, leftEyebrow, rightEye, leftEye) {
+    return {
+      "bodyType": bodyType,
+      "bgColor": "#cccccc",
+      "version": "0.0.1",
+      "layers": [
+        {
+          "part": "head",
+          "index": 0,
+          "shading": 0
+        },
+        {
+          "part": "mouth",
+          "index": mouth,
+          "shading": 0,
+          "base": "#990000"
+        },
+        {
+          "part": "eyebrows",
+          "index": leftEyebrow,
+          "shading": 0
+        },
+        {
+          "part": "eyebrows",
+          "index": rightEyebrow,
+          "shading": 0,
+          "flip": true
+        },
+        {
+          "part": "eyes",
+          "index": leftEye,
+          "shading": 0
+        },
+        {
+          "part": "eyes",
+          "index": rightEye,
+          "shading": 0,
+          "flip": true
+        }
+      ]
+    }
+  }
+  const buildSymetricalFace = function(bodyType, mouth, eyebrow, eye) {
+    return buildFace(bodyType, mouth, eyebrow, eyebrow, eye, eye)
+  }
+  const buildForEachMouth = function(bodyType) {
+    return ranges.mouth.map(mouth => buildSymetricalFace(bodyType, mouth, ranges.eyebrows[0], ranges.eyes[0]));
+  }
+  return { bodyTypes, ranges, buildFace, buildSymetricalFace, buildForEachMouth };
+});
---
+++ poc/scripts/poc.js
@@ -0,0 +1,11 @@
+namespace("fm.poc.ProofOfConcept", {}, () => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    render() {
+      return (<></>);
+    }
+  };
+});
