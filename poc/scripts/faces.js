3145a092eff5d9d6db3bf83cd40a509976d1501d
Daniel Johnson <voltron42@users.noreply.github.com>
2025-03-13 21:08:33 +0000

    building out proof of concept

--- poc/scripts/faces.js
+++ poc/scripts/faces.js
@@ -1,15 +1,23 @@
 namespace("fm.poc.Faces", {}, () => {
+  const version = "0.0.1";
   const bodyTypes = ["fit","superman","hulk","woman"];
   const ranges = {
     eyebrows: [27,47],
     eyes: [41,48],
     mouth: [0,26]
   }
+  const range = function([ min, max ]) {
+    const outval = [];
+    for(let i = min; i <= max; i++) {
+      outval.push(i);
+    }
+    return outval;
+  }
   const buildFace = function(bodyType, mouth, rightEyebrow, leftEyebrow, rightEye, leftEye) {
     return {
       "bodyType": bodyType,
       "bgColor": "#cccccc",
-      "version": "0.0.1",
+      "version": version,
       "layers": [
         {
           "part": "head",
@@ -51,19 +59,26 @@ namespace("fm.poc.Faces", {}, () => {
     return buildFace(bodyType, mouth, eyebrow, eyebrow, eye, eye)
   }
   const buildForEachMouth = function(bodyType) {
-    return ranges.mouth.map(mouth => buildSymetricalFace(bodyType, mouth, ranges.eyebrows[0], ranges.eyes[0]));
+    return range(ranges.mouth).map(mouth => buildSymetricalFace(bodyType, mouth, ranges.eyebrows[0], ranges.eyes[0]));
   }
   const buildForAllSymetricalEyes = function(bodyType) {
-    return ranges.eyebrows.reduce((acc, eyebrow) => acc.concat(ranges.eyes.map(eye => buildSymetricalFace(bodyType, ranges.mouths[0], eyebrow, eye))), []);
+    return range(ranges.eyebrows).reduce((acc, eyebrow) => acc.concat(range(ranges.eyes).map(eye => buildSymetricalFace(bodyType, ranges.mouth[0], eyebrow, eye))), []);
   }
   const buildForAllMixedEyes = function(bodyType) {
-    return ranges.eyebrows.reduce((acc,leftEyebrow) => {
-      return acc.concat(ranges.eyebrows.reduce((acc, rightEyebrow) => {
-        return acc.concat(ranges.eyes.reduce((acc, leftEye) => {
-          return acc.concat(ranges.eyes.map(rightEye => buildFace(bodyType, ranges.mouths[0], rightEyebrow, leftEyebrow, rightEye, leftEye)));
-        }), acc);
-      }), acc);
-    }, []);
+    const eyebrows = range(ranges.eyebrows);
+    const eyes = range(ranges.eyes);
+    console.log({ eyebrows, eyes });
+    const outval = [];
+    eyebrows.forEach((leftEyebrow) => {
+      eyebrows.forEach((rightEyebrow) => {
+        eyes.forEach((leftEye) => {
+          eyes.forEach((rightEye) => {
+            outval.push(buildFace(bodyType, ranges.mouth[0], rightEyebrow, leftEyebrow, rightEye, leftEye));
+          });
+        });
+      });
+    });
+    return outval;
   }
-  return { bodyTypes, ranges, buildFace, buildSymetricalFace, buildForEachMouth, buildForAllSymetricalEyes, buildForAllMixedEyes };
+  return { version, bodyTypes, ranges, buildFace, buildSymetricalFace, buildForEachMouth, buildForAllSymetricalEyes, buildForAllMixedEyes };
 });
--- poc/scripts/poc.js
+++ poc/scripts/poc.js
@@ -1,19 +1,70 @@
-namespace("fm.poc.ProofOfConcept", {}, () => {
+namespace("fm.poc.ProofOfConcept", {
+  'gizmo-atheneum.namespaces.paper-doll.Dataset': 'Dataset',
+  "fm.poc.Faces": "Faces",
+  "fm.common.ProgressBar": "ProgressBar"
+}, ({ Dataset, Faces, ProgressBar }) => {
+  const [percentOfScreenWidth, percentOfScreenHeight] = [ 0.2, 0.5];
+  const bodyType = "woman";
+  const images = {
+    mouths: Faces.buildForEachMouth(bodyType),
+    symetricalEyes: Faces.buildForAllSymetricalEyes(bodyType),
+    mixedEyes: Faces.buildForAllMixedEyes(bodyType)
+  }
+  console.log({ images });
   return class extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        bodyType: "woman"
-      };
+      this.state = { bodyType, images };
     }
-    afterRender() {
-      
+    componentDidMount() {
+      if (!this.state.metadata) {
+        Dataset.load(this.state.bodyType, Faces.version, percentOfScreenWidth, percentOfScreenHeight, (dataset) => {
+          this.setState({ metadata: dataset, progress: undefined, selectedLayer: 0});
+        }, (resp) => {
+          console.log(resp);
+          throw resp;
+        }, (state) => {
+          const progress = (100 * (state.state + 1)) / (state.max + 1);
+          this.setState({progress})
+        });
+      }
     }
     render() {
-      return (<>
-        { this.state.metadata && (<>
-        </>)}
-      </>);
+      if (this.state.progress) {
+        return <>
+          <ProgressBar subject="display faces" progress={this.state.progress}/>
+        </>;
+      } else if (this.state.metadata) {
+        return (<div className="d-flex flex-column justify-content-center">
+          <div className="d-flex justify-content-center"><h2>Mouths</h2></div>
+          <div className="d-flex flex-wrap justify-content-center">
+            { this.state.images.mouths.map((schematic) => {
+              const { width, height, content, viewBox } = this.state.metadata.drawSVG(schematic);
+              return <div>
+                <svg width={width} height={height} viewBox={viewBox} dangerouslySetInnerHTML={{ __html: content }}></svg>
+              </div>
+            })}
+          </div>
+          <div className="d-flex justify-content-center"><h2>Symetrical Eyes</h2></div>
+          <div className="d-flex flex-wrap justify-content-center">
+            { this.state.images.symetricalEyes.map((schematic) => {
+              const { width, height, content, viewBox } = this.state.metadata.drawSVG(schematic);
+              return <div>
+                <svg width={width} height={height} viewBox={viewBox} dangerouslySetInnerHTML={{ __html: content }}></svg>
+              </div>
+            })}
+          </div>
+          <div className="d-flex justify-content-center"><h2>Mixed Eyes</h2></div>
+          <div className="d-flex flex-wrap justify-content-center">
+            { this.state.images.mixedEyes.map((schematic) => {
+              const { width, height, content, viewBox } = this.state.metadata.drawSVG(schematic);
+              return <div>
+                <svg width={width} height={height} viewBox={viewBox} dangerouslySetInnerHTML={{ __html: content }}></svg>
+              </div> 
+            })}
+          </div>
+        </div>)
+      }
     }
   };
 });
